b11cf7e15e3260cd1c67b3804fedc4e10fa465f4
Dmytro Khylko <dmytro.khylko@gmail.com>
2021-11-28 09:10:16 +0200

    Lab8_code

---
+++ Lab8/src/app/services/favorite-voter.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Voter } from './voter.service';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteVoterService {
+  public favoriteVoters$ = new BehaviorSubject<Voter[]>([]);
+  public favoriteVoters: Voter[] = [];
+
+  constructor() { }
+
+
+  isFavorite(voterToFind: Voter) {
+    for (const voter of this.favoriteVoters) {
+      if (voter.uuid === voterToFind.uuid) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addFavoriteVoter(voterToAdd: Voter): void {
+    this.favoriteVoters.push(voterToAdd);
+
+    console.log("service", this.favoriteVoters);
+    this.favoriteVoters$.next(this.favoriteVoters)
+
+  }
+  
+  removeFavoriteVoters(voterToRemove: Voter): void {
+    this.favoriteVoters = this.favoriteVoters.filter(value => value.uuid != voterToRemove.uuid)
+    this.favoriteVoters$.next(this.favoriteVoters)
+  }
+
+  getFavoriteVoters(): Voter[] {
+    return this.favoriteVoters;
+  }
+}
